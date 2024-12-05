507a4dc1608b17a1a2acc2bd56a9eb49b1dd3c09
yideng <2219298698@qq.com>
2024-12-05 15:56:52 +0800

    fill insighter

--- LLM_BASE/insighter.py
+++ LLM_BASE/insighter.py
@@ -1,9 +1,34 @@
-class Insighter:
-    def __init__(self, data):
-        self.data = data
-        self.insight = None
+from LLM import ChatGPTFunction
+from utils import load_file_from_cwd,save_file_to_cwd
 
+class Insighter:
+    def __init__(self):        
+        self.AK = None
+        self.insight = None       
+        self.config = load_file_from_cwd('config.json')
+        self.llm = ChatGPTFunction(**self.config)
+        
     def generate_insight(self,history):
         # Some logic to generate insight
-        self.insight = "Some insight"
-        return "some insight"
+        insight_template = load_file_from_cwd("inference_principle.txt")
+        insight_template.replace('[history]',history)
+        messages = [
+            {
+                'role': 'system',
+                'content': insight_template
+            }
+        ]
+        self.llm.change_messages(messages)
+        response,_ = self.llm.parse([],0)
+        self.insight = response
+        return self.insight
+    
+    def get_knowledge(self):
+        return self.AK
+    
+    def set_knowledge(self,AK):
+        self.AK = AK
+        
+    def save_knowledge(self):
+        save_file_to_cwd(self.AK,'AK.txt')
+    
